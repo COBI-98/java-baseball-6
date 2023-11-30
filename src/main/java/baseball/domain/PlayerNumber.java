d618909c300a9e62b33bf17185fcaf81b7857072
COBI-98 <tkdgus968@naver.com>
2023-11-30 18:57:19 +0900

    refactor: 에러메시지 상수처리 수정

--- src/main/java/baseball/domain/PlayerNumber.java
+++ src/main/java/baseball/domain/PlayerNumber.java
@@ -1,5 +1,10 @@
 package baseball.domain;
 
+import static baseball.message.ErrorMessages.INVALID_DUPLICATION_NUMBER;
+import static baseball.message.ErrorMessages.INVALID_NUMBER_FORMAT;
+import static baseball.message.ErrorMessages.INVALID_NUMBER_RANGE;
+
+import baseball.message.ErrorMessages;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,6 +13,7 @@ import java.util.stream.IntStream;
 public record PlayerNumber(String number) {
 
     private static int MAX_NUMBER_COUNT = 3;
+
     public PlayerNumber {
         validateRangeFromNumber(number);
         validateCharacterFromNumber(number);
@@ -15,15 +21,15 @@ public record PlayerNumber(String number) {
     }
 
     public static void validateRangeFromNumber(String word) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.(입력값 3자리수)");
         if (word.length() != MAX_NUMBER_COUNT) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE.getMessage());
         }
     }
 
     public static void validateCharacterFromNumber(String word) {
         if (!IntStream.range(0, word.length()).
                 allMatch(index -> Character.isDigit(word.charAt(index)))) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.(자연수가 아닌 수가 존재합니다.)");
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT.getMessage());
         }
     }
 
@@ -33,7 +39,7 @@ public record PlayerNumber(String number) {
                 .collect(Collectors.toSet());
 
         if (set.size() != word.length()) {
-            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.(중복된 숫자가 존재합니다.)");
+            throw new IllegalArgumentException(INVALID_DUPLICATION_NUMBER.getMessage());
         }
     }
 }
--- src/main/java/baseball/message/ErrorMessages.java
+++ src/main/java/baseball/message/ErrorMessages.java
@@ -2,7 +2,10 @@ package baseball.message;
 
 public enum ErrorMessages {
     INVALID_INPUT_FORMAT("[ERROR] 숫자만 입력 가능합니다."),
-    INVALID_INPUT_RETRY_FORMAT("[ERROR] 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    INVALID_INPUT_RETRY_FORMAT("[ERROR] 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요"),
+    INVALID_NUMBER_RANGE("잘못된 값을 입력하셨습니다.(입력값 3자리수)"),
+    INVALID_NUMBER_FORMAT("잘못된 값을 입력하셨습니다.(자연수가 아닌 수가 존재합니다.)"),
+    INVALID_DUPLICATION_NUMBER("잘못된 값을 입력하셨습니다.(중복된 숫자가 존재합니다.)") ;
 
     private String message;
 
