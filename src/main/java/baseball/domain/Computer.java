ef7d02ef36f960f53149c628021f3f5f73322263
COBI-98 <tkdgus968@naver.com>
2023-11-30 19:50:30 +0900

    refactor: 컴퓨터가 입력한 숫자에 대한 숫자를 비교하도록 변경

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,8 +1,13 @@
 package baseball.domain;
 
 import baseball.util.ComputerRandomGameNumber;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Computer {
+
+    private static final int NUMBER_LENGTH = 3;
 
     private String computerGameNumber;
 
@@ -16,5 +21,49 @@ public class Computer {
     public String getComputerGameNumber() {
         return computerGameNumber;
     }
-    
+
+
+    public List<Integer> calculateBallAndStrikeCount(Player player) {
+        List<Integer> ballAndStrikeCountList = new ArrayList<>();
+        int ballIndex = calculateBallCount(computerGameNumber, player.getPlayerNumber());
+        int strikeIndex = calculateStrikeCount(computerGameNumber, player.getPlayerNumber());
+        ballAndStrikeCountList.add(ballIndex);
+        ballAndStrikeCountList.add(strikeIndex);
+
+        return ballAndStrikeCountList;
+    }
+
+    private int calculateBallCount(String computerNumber, String playerNumber) {
+        int ballCount = 0;
+        for (int computerPlace = 0; computerPlace < NUMBER_LENGTH; computerPlace++) {
+            if (isBallCheck(computerNumber.charAt(computerPlace), computerPlace, playerNumber)){
+                ballCount++;
+            }
+        }
+
+        return ballCount;
+    }
+
+    private boolean isBallCheck(char computerPlaceNumber, int computerPlace, String playerNumber) {
+        for (int playerPlace = 0; playerPlace < NUMBER_LENGTH; playerPlace++) {
+            if (computerPlaceNumber == playerNumber.charAt(playerPlace) && computerPlace != playerPlace) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int calculateStrikeCount(String computerGameNumber, String playerNumber) {
+        int strikeCount = 0;
+        for (int place = 0; place < NUMBER_LENGTH; place++) {
+            if (isStrikeCheck(computerGameNumber.charAt(place), playerNumber.charAt(place))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    private boolean isStrikeCheck(char computerPlaceNumber, char playerPlaceNumber) {
+        return computerPlaceNumber == playerPlaceNumber;
+    }
 }
--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -2,14 +2,18 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
 import baseball.util.ComputerRandomGameNumber;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ComputerTest {
+
+    private static final int BALL_INDEX = 0;
+    private static final int STRIKE_INDEX = 1;
 
     @DisplayName("constructor() : 컴퓨터는 3자리의 수를 생성한다.")
     @Test
@@ -19,5 +23,33 @@ class ComputerTest {
 
         //when //then
         assertThat(computer.getComputerGameNumber()).isEqualTo("123");
+    }
+
+    @DisplayName("calculateBallAndStrikeCount() : 컴퓨터가 사용자의 자리 수를 비교한다. (볼)")
+    @ParameterizedTest
+    @CsvSource({"123,0", "456,0", "345,1", "314,2", "312,3"})
+    void gameNumberPlaceValidateBall(String playerNumber, int ballCount) throws Exception {
+        //given
+        Computer computer = new Computer(new ComputerRandomGameNumber(List.of(1, 2, 3)));
+        Player player = new Player(playerNumber);
+        //when
+        List<Integer> ballAndStrikeList = computer.calculateBallAndStrikeCount(player);
+
+        //then
+        Assertions.assertThat(ballAndStrikeList.get(BALL_INDEX)).isEqualTo(ballCount);
+    }
+
+    @DisplayName("컴퓨터와 사용자의 자리 수를 비교해 숫자를 비교한다. (스트라이크)")
+    @ParameterizedTest
+    @CsvSource({"456,0", "453,1", "423,2", "123,3"})
+    void gameNumberPlaceValidateStrike(String playerNumber, int strikeCount) throws Exception {
+        //given
+        Computer computer = new Computer(new ComputerRandomGameNumber(List.of(1, 2, 3)));
+        Player player = new Player(playerNumber);
+        //when
+        List<Integer> ballAndStrikeList = computer.calculateBallAndStrikeCount(player);
+
+        //then
+        Assertions.assertThat(ballAndStrikeList.get(STRIKE_INDEX)).isEqualTo(strikeCount);
     }
 }
