1c72062f5dfe5def4f00cad3edf5d197ff40e47a
COBI-98 <tkdgus968@naver.com>
2023-12-01 16:15:13 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/baseball/domain/HintResult.java
+++ src/main/java/baseball/domain/HintResult.java
@@ -13,18 +13,6 @@ public enum HintResult {
         this.title = title;
     }
 
-    public static HintResult findByHintResult(String title){
-
-        return Arrays.stream(HintResult.values())
-                .filter(hintResult -> hintResult.hasTitle(title))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("일치하지않는 형식"));
-    }
-
-    private boolean hasTitle(String hintTitle) {
-        return title.equals(hintTitle);
-    }
-
     public String getTitle() {
         return title;
     }
